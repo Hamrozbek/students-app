c634ebaa0d3962a3f1182cd4b713de896a0cef20
Hamrozbek <hamroznuriddinov41@gmail.com>
2025-07-23 12:13:45 +0500

    start app

--- src/modules/StudentsList.jsx
+++ src/modules/StudentsList.jsx
@@ -11,7 +11,7 @@ const StudentsList = () => {
                     <h2 className='font-semibold text-[20px] mb-2'>{item.name} - {item.surname}</h2>
                     <p className='font-semibold text-[18px]'>Yosh: {item.age}</p>
                     <p className='font-normal text-[18px]'>Manzil: {item.region} </p>
-                    <button onClick={() => collectSavedStudents(item)} className='bg-[#2f4f4f] cursor-pointer w-full mt-2 p-2 rounded-md text-white font-semibold'>Saved</button>
+                    <button onClick={() => collectSavedStudents(item)} className='bg-[#000000] cursor-pointer w-full mt-2 p-2 rounded-md text-white font-semibold'>Saved</button>
                 </div>
             ))}
         </div>
